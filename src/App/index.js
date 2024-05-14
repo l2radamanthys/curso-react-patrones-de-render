a51127907069af9ee2a660556fbb27c2f6075441
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2024-05-14 01:07:26 -0300

    React.Context

--- src/App/index.js
+++ src/App/index.js
@@ -1,70 +1,14 @@
 import React from 'react';
 import { AppUI } from './AppUI';
+import { TodoProvider } from '../TodoContext';
 
-import { useLocalStorage } from './useLocalStorage';
-
-// const defaultTodos = [
-//   { text: 'Cortar cebolla', completed: true },
-//   { text: 'Tomar el Curso de Intro a React.js', completed: false },
-//   { text: 'Llorar con la Llorona', completed: false },
-//   { text: 'LALALALALA', completed: false },
-//   { text: 'Usar estados derivados', completed: true },
-// ];
-
-// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
-// localStorage.removeItem('TODOS_V1');
 
 function App() {
-  const { 
-    item: todos, 
-    setItem: setTodos, 
-    loading, 
-    error
-  } = useLocalStorage("TODOS_V1", []);
-  const [searchValue, setSearchValue] = React.useState('');
-
-  const completedTodos = todos.filter(
-    todo => !!todo.completed
-  ).length;
-  const totalTodos = todos.length;
-
-  const searchedTodos = todos.filter(
-    (todo) => {
-      const todoText = todo.text.toLowerCase();
-      const searchText = searchValue.toLowerCase();
-      return todoText.includes(searchText);
-    }
+  return (
+    <TodoProvider> 
+      <AppUI />
+    </TodoProvider>
   );
-
-  const completeTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
-  };
-
-  const deleteTodo = (text) => {
-    const newTodos = [...todos];
-    const todoIndex = newTodos.findIndex(
-      (todo) => todo.text === text
-    );
-    newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
-  };
-  
-  return <AppUI
-    loading={loading}
-    error={error}
-    completedTodos={completedTodos}
-    totalTodos={totalTodos}
-    searchValue={searchValue}
-    setSearchValue={setSearchValue}
-    searchedTodos={searchedTodos}
-    completeTodo={completeTodo}
-    deleteTodo={deleteTodo}
-  />
 }
 
 export default App;
