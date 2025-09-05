72fedbb1c1c905f6f471f0d96cd546d4254f0888
Ricardo D. Quiroga <l2radamanthys@gmail.com>
2025-09-04 22:13:58 -0300

    Hight order Components ejemplos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,46 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+
+function App(props) {
+  return (
+    <h1>{props.saludo}, {props.nombre}!</h1>
+  )
+}
+
+// function withWhatever(WrappedComponent) {
+//   return function ComponenteDeVerdad(props) {
+//     return (
+//       <React.Fragment>
+//         <WrappedComponent {...props}/>
+//         <p>Estamos acompañando al WrappedComponent</p>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+function withSaludo(WrappedComponent) {
+  return function WrappedComponentWithSaludo(saludo) {
+    return function ComponenteDeVerdad(props) {
+      return (
+        <React.Fragment>
+          <WrappedComponent {...props} saludo={saludo}/>
+          <p>Estamos acompañando al WrappedComponent</p>
+        </React.Fragment>
+      );
+    }
+  }
+}
+
+// const AppWithWhatever = withWhatever(App);
+const AppWithSaludo = withSaludo(App)('Holis');
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+  <AppWithSaludo saludo="Hey" nombre="Marc" />
+  // <App 
+  //   saludo="Buenas"
+  //   nombre="Juanito"
+  // />
+);
